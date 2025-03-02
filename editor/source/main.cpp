60b6b8663c9977fe66e02a971b3400696504c0f1
Berkay <berkaysahindev@gmail.com>
2025-03-02 21:18:45 +0300

    json editor for entities

--- editor/source/main.cpp
+++ editor/source/main.cpp
@@ -1,19 +1,18 @@
-#include "raylib.h"
+#include <iostream>
+#include <fstream>
+#include <filesystem>
 
+#include "raylib.h"
 #include "imgui.h"
 #include "rlImGui.h"
 
-#include <iostream>
-#include <filesystem>
-#include <fstream>
-
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-#include <rapidjson/document.h>
-#include <rapidjson/stringbuffer.h>
 #include "rapidjson/prettywriter.h"
-#include <imgui.h>
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
 
 namespace fs = std::filesystem;
 
@@ -27,67 +26,212 @@ void SaveJsonToFile(const fs::path& filePath, const rapidjson::Document& documen
     rapidjson::StringBuffer buffer;
     rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
     document.Accept(writer);
-    
-    outFile << buffer.GetString();  
+
+    outFile << buffer.GetString();
     outFile.close();
 }
 
-void ShowJsonEditor(const fs::path& filePath) {
+bool LoadJsonFromFile(const fs::path& filePath, rapidjson::Document& document) {
     std::ifstream inFile(filePath);
     if (!inFile) {
         std::cerr << "Error: Unable to open file: " << filePath << std::endl;
-        return;
+        return false;
     }
 
     std::string jsonString((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
     inFile.close();
 
-    rapidjson::Document document;
     document.Parse(jsonString.c_str());
-
     if (document.HasParseError()) {
-        std::cerr << "Error parsing JSON: " << document.GetParseError() << std::endl;
+        std::cerr << "Error parsing JSON in file: " << filePath << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void RenderJsonObject(rapidjson::Value& object, const fs::path& filePath, rapidjson::Document& document) {
+    for (auto& member : object.GetObject()) {
+        std::string key = member.name.GetString();
+        rapidjson::Value& value = member.value;
+
+        ImGui::PushID(key.c_str());
+
+        if (value.IsInt()) {
+            int intValue = value.GetInt();
+            if (ImGui::InputInt(("Int: " + key).c_str(), &intValue)) {
+                value.SetInt(intValue);
+                SaveJsonToFile(filePath, document);
+            }
+        }
+        else if (value.IsFloat()) {
+            float floatValue = value.GetFloat();
+            if (ImGui::InputFloat(("Float: " + key).c_str(), &floatValue, 0.1f, 1.0f, "%.3f")) {
+                value.SetFloat(floatValue);
+                SaveJsonToFile(filePath, document);
+            }
+        }
+        else if (value.IsBool()) {
+            bool boolValue = value.GetBool();
+            if (ImGui::Checkbox(("Bool: " + key).c_str(), &boolValue)) {
+                value.SetBool(boolValue);
+                SaveJsonToFile(filePath, document);
+            }
+        }
+        else if (value.IsString()) {
+            std::string strValue = value.GetString();
+            char buffer[256];  
+            strncpy(buffer, strValue.c_str(), sizeof(buffer));
+            buffer[sizeof(buffer) - 1] = '\0';
+
+            if (ImGui::InputText(("String: " + key).c_str(), buffer, sizeof(buffer))) {
+                value.SetString(buffer, document.GetAllocator());
+                SaveJsonToFile(filePath, document);
+            }
+        }
+        else if (value.IsArray()) {
+            if (ImGui::CollapsingHeader(("Array: " + key).c_str())) {
+                ImGui::Indent();
+                for (rapidjson::SizeType i = 0; i < value.Size(); ++i) {
+                    ImGui::PushID(i);
+
+                    rapidjson::Value& item = value[i];
+
+                    if (item.IsObject()) {
+                        if (ImGui::CollapsingHeader(("Item " + std::to_string(i)).c_str())) {
+                            RenderJsonObject(item, filePath, document);
+                        }
+                    } 
+                    else if (item.IsInt()) {
+                        int intValue = item.GetInt();
+                        if (ImGui::InputInt(("Item " + std::to_string(i)).c_str(), &intValue)) {
+                            item.SetInt(intValue);
+                            SaveJsonToFile(filePath, document);
+                        }
+                    } 
+                    else if (item.IsFloat()) {
+                        float floatValue = item.GetFloat();
+                        if (ImGui::InputFloat(("Item " + std::to_string(i)).c_str(), &floatValue)) {
+                            item.SetFloat(floatValue);
+                            SaveJsonToFile(filePath, document);
+                        }
+                    } 
+                    else if (item.IsBool()) {
+                        bool boolValue = item.GetBool();
+                        if (ImGui::Checkbox(("Item " + std::to_string(i)).c_str(), &boolValue)) {
+                            item.SetBool(boolValue);
+                            SaveJsonToFile(filePath, document);
+                        }
+                    } 
+                    else if (item.IsString()) {
+                        std::string strValue = item.GetString();
+                        char buffer[256];  
+                        strncpy(buffer, strValue.c_str(), sizeof(buffer));
+                        buffer[sizeof(buffer) - 1] = '\0';
+
+                        if (ImGui::InputText(("Item " + std::to_string(i)).c_str(), buffer, sizeof(buffer))) {
+                            item.SetString(buffer, document.GetAllocator());
+                            SaveJsonToFile(filePath, document);
+                        }
+                    }
+
+                    ImGui::PopID();
+                }
+                ImGui::Unindent();
+            }
+        }
+        else if (value.IsObject()) {
+            if (ImGui::CollapsingHeader(("Object: " + key).c_str())) {
+                ImGui::Indent();
+                RenderJsonObject(value, filePath, document);
+                ImGui::Unindent();
+            }
+        }
+
+        ImGui::PopID();
+    }
+}
+
+void RenderVariant(rapidjson::Value& variant, const fs::path& filePath, rapidjson::Document& document, int index) {
+    const char* type = variant["type"].GetString();
+
+    ImGui::PushID(index);
+    ImGui::Indent();
+
+    if (ImGui::CollapsingHeader(type)) {
+        if (variant.HasMember("value")) {
+            RenderJsonObject(variant["value"], filePath, document);
+        }
+    }
+
+    ImGui::Unindent();
+    ImGui::PopID();
+}
+
+void ShowJsonEditor(const fs::path& folderPath) {
+    static char newEntityName[128] = "";
+    static bool showNewEntityPopup = false;
+
+    if (ImGui::Button("Create New Entity")) {
+        showNewEntityPopup = true;
+    }
+
+    if (showNewEntityPopup) {
+        ImGui::OpenPopup("New Entity");
+    }
+
+    if (ImGui::BeginPopupModal("New Entity", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::InputText("Entity Name", newEntityName, sizeof(newEntityName));
+        if (ImGui::Button("Create")) {
+            fs::path filePath = folderPath / (std::string(newEntityName) + ".entity");
+
+            rapidjson::Document newDoc;
+            newDoc.SetObject();
+            rapidjson::Document::AllocatorType& allocator = newDoc.GetAllocator();
+
+            newDoc.AddMember("id", 123456789, allocator);
+
+            rapidjson::Value variants(rapidjson::kArrayType);
+            newDoc.AddMember("variants", variants, allocator);
+
+            SaveJsonToFile(filePath, newDoc);
+            ImGui::CloseCurrentPopup();
+            showNewEntityPopup = false;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Cancel")) {
+            showNewEntityPopup = false;
+        }
+        ImGui::EndPopup();
+    }
+
+    if (!fs::exists(folderPath) || !fs::is_directory(folderPath)) {
+        std::cerr << "Error: Folder does not exist or is not a directory: " << folderPath << std::endl;
         return;
     }
 
-    if (ImGui::CollapsingHeader("Entity")) {
-        const rapidjson::Value& variants = document["variants"];
-        for (rapidjson::SizeType i = 0; i < variants.Size(); ++i) {
-            const rapidjson::Value& variant = variants[i];
-            const char* type = variant["type"].GetString();
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".entity") {
+            fs::path filePath = entry.path();
+            std::string fileName = filePath.stem().string();
 
-            ImGui::Indent();
-            if (ImGui::CollapsingHeader((std::string(type) + "##" + std::to_string(i)).c_str())) {
-                const rapidjson::Value& value = variant["value"];
-                for (auto& member : value.GetObject()) {
-                    if (member.value.IsInt()) {
-                        int intValue = member.value.GetInt();
-                        std::string key = member.name.GetString();
-                        
-                        if (ImGui::InputInt(("Value: " + key + "##" + std::to_string(i)).c_str(), &intValue)) {
-                            const_cast<rapidjson::Value&>(member.value).SetInt(intValue);
-                            SaveJsonToFile(filePath, document);  // Save after every edit
-                        }
+            rapidjson::Document document;
+            if (!LoadJsonFromFile(filePath, document)) {
+                continue;
+            }
+
+            ImGui::PushID(fileName.c_str());
+            if (ImGui::CollapsingHeader(fileName.c_str())) {
+                if (document.HasMember("variants")) {
+                    rapidjson::Value& variants = document["variants"];
+                    for (rapidjson::SizeType i = 0; i < variants.Size(); ++i) {
+                        RenderVariant(variants[i], filePath, document, i);
                     }
                 }
             }
-            ImGui::Unindent();
+            ImGui::PopID();
         }
     }
-}
-
-
-float ScaleToDPIF(float value)
-{
-    return GetWindowScaleDPI().x * value;
-}
-
-int ScaleToDPII(int value)
-{
-    return int(GetWindowScaleDPI().x * value);
-}
-
-void DisplayJson(rapidjson::Value& jsonValue) {
 }
 
 int main(int argc, char* argv[])
@@ -114,7 +258,7 @@ int main(int argc, char* argv[])
 
         if (ImGui::Begin("Entity List", &open))
 		{
-            ShowJsonEditor("../shared/player.entity");
+            ShowJsonEditor("../shared/");
 		}
 
 
--- todo.md
+++ todo.md
@@ -1,15 +1,17 @@
+* implement dummy type files for editor
+* Implemenet a long entity for sample
+* Implement adding variants to entity
+* Implement adding elements to array
+
+-----------------------------------------
+
+* implement component ref, entity ref
+* implement a way to refer other entities
+* implement raylib layer
+
 * [DONE] remove component class, use variant instead
 * [DONE] change folder names
 * [DONE] implemenet .entity file serialization/deserialization. entity has an id and list of variants
 * [DONE] improve api
-* add update methods to variants ()
-* [PARTIALLYDONE] implement dummy type files for editor
-
-* Start editor layer
-* Implemenet a long entity for sample
-
-
-* implement component ref, entity ref
-* implement a gui system for entities
-* implement a way to refer other entities
-* implement raylib layer
+* [DONE] add update methods to variants ()
+* [DONE] Start editor layer
