4c8c34d708384c7caca543fb5424ba72106dfdd0
Berkay <berkaysahindev@gmail.com>
2025-04-19 22:07:42 +0300

    updates shared resources

--- engine/include/variant/variant_base.h
+++ engine/include/variant/variant_base.h
@@ -21,6 +21,7 @@ struct VariantBase {
     virtual void on_post_init() {}
     virtual void on_update() {}
     virtual void on_play_start() {}
+    virtual void on_play_late_start() {}
     virtual void on_play_update() {}
 
     virtual bool check_dependencies(const std::string& method_name) const { return true; }
