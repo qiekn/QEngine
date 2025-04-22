fbad6f8ff625a9dc5666902732630ddf57cf76d2
Berkay <berkaysahindev@gmail.com>
2025-04-22 14:46:34 +0300

    removes raylib/src as under 3rdparty, as linking + include is enough

--- editor/premake5.lua
+++ editor/premake5.lua
@@ -13,8 +13,8 @@ workspace "ZeytinEditor"
         "include",
         "3rdparty/imgui",
         "3rdparty/zmq",
-        "3rdparty/raylib/src",
-        "3rdparty/rapidjson/include"
+        "3rdparty/rapidjson/include",
+        "3rdparty/raylib/raylib-5.5_linux_amd64/include"
     }
     
     filter "system:windows"
