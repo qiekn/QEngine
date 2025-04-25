8730203fa84cec9c707d23f9ac9321d1a3003485
Berkay <berkaysahindev@gmail.com>
2025-04-25 22:52:06 +0300

    updates according to new path

--- engine/scripts/standalone_build.sh
+++ engine/scripts/standalone_build.sh
@@ -8,4 +8,4 @@
 #    rm -r build
 #fi
 
-premake5 gmake && cd build && make config=standalone platform=x64 && cd ..
+python3 parser2.py && cd .. && premake5 gmake && cd build && make config=standalone
