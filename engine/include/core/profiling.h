8d54f24f484e1bbbbb2c26c898aa2c96d5fb3d45
Berkay <berkaysahindev@gmail.com>
2025-04-26 16:44:16 +0300

    adds profiling.h

---
+++ engine/include/core/profiling.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef ZPROFILE
+    #include "tracy/Tracy.hpp"
+    
+    #define ZPROFILE_ZONE() ZoneScoped
+    #define ZPROFILE_ZONE_NAMED(name) ZoneScopedN(name)
+    #define ZPROFILE_FUNCTION() ZoneScoped
+    #define ZPROFILE_TEXT(text, size) ZoneText(text, size)
+    #define ZPROFILE_VALUE(value) ZoneValue(value)
+#else
+    #define ZPROFILE_ZONE()
+    #define ZPROFILE_ZONE_NAMED(name)
+    #define ZPROFILE_FUNCTION()
+    #define ZPROFILE_TEXT(text, size)
+    #define ZPROFILE_VALUE(value)
+#endif
